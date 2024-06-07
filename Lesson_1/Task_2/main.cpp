1b6e25167bf6a0b9cb8a8d0ff62834c3c6a3d32f
Your Name <you@example.com>
2024-06-07 14:14:54 +0300

    Addet second task for first lection

---
+++ Lesson_1/Task_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout <<"enter the x"<< endl;
+    cin >> x;
+    cout <<"enter the y"<< endl;
+    cin >> y;
+    int sum = x+y;
+    cout << sum << endl;
+
+    return 0;
+}
