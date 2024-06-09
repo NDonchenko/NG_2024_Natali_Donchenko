95671d9b6b1f52d2ea390f9df3034ec1e72a6625
Your Name <you@example.com>
2024-06-09 22:48:09 +0300

    Addet third task for first lection

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -7,28 +7,19 @@ int main()
     int salary = 0;
     cout << "how much do you earn?" << endl;
     cin>> salary;
-    int CheckThousand = (1000>salary) - (1000<salary);
-    if (CheckThousand>0) {
+
+    if (salary>999) {
+        //cout << " ok, but you can do better)))"<<endl;
+        if (salary>999999){
+            cout << " o it turns out you're a millionaire? Handsome!!!"<<endl;
+        }
+
+        if (salary<1000000) {
+            cout << " ok, but you can do better)))"<<endl;
+        }
+    }
+    if (salary<1000){
         cout << " work harder! "<<endl;
-    }
-    if (CheckThousand<0) {
-        int CheckMillion = (1000000>salary) - (1000000<salary);
-        if (CheckMillion>0){
-            cout << " ok, but you can do better)))"<<endl;
-        }
-        if (CheckMillion<0){
-            cout << " so you are rich, will you borrow half a million?"<<endl;
-        }
-        if (CheckMillion<1){
-            if(CheckMillion>-1){
-                cout << " o it turns out you're a millionaire? Handsome!!!"<<endl;
-            }
-        }
-    }
-    if (CheckThousand<1) {
-        if (CheckThousand>-1) {
-            cout << " ok, but you can do better)))"<<endl;
-         }
     }
     cout << "but you are great, mom loves you in any way, even poor:)))"<<endl;
     return 0;
