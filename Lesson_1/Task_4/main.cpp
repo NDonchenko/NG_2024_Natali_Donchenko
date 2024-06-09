c50783b1d0feb4f178ee147898b4dc7caf6c3ab0
Your Name <you@example.com>
2024-06-09 19:16:58 +0300

    Addet fourth task for first lection

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -6,49 +6,37 @@ using namespace std;
 int main()
 {
     int operation = 0;
-    double num1;
-    double num2;
-    bool ToContinue = true;
-    do {
-        cout << "select an operation" << endl;
-        cout << "1.sum" << endl;
-        cout << "2.difference" << endl;
-        cout << "3.multiplication" << endl;
-        cout << "4.division" << endl;
-        cout << "5.square" << endl;
-        cout << "6.square root" << endl;
-        cout << "7.exit" << endl;
-        cin >> operation;
-        if (operation != 7){
-            cout << "Enter the numbers with which you want to perform the operation:" << endl;
-            cin >> num1 >> num2;
-        }
-        switch (operation) {
-            case 1 :
-                cout << "sum: "<< num1 + num2 << endl;
-                break;
-            case 2 :
-                cout << "difference: " << num1 - num2 << endl;
-                break;
-            case 3 :
-                cout << "multiplication: " <<num1 * num2 << endl;
-                break;
-            case 4 :
-                cout << "division: " << num1 / num2 << endl;
-                break;
-            case 5 :
-                cout << "square: " << pow(num1,2) <<";" << pow(num2,2) << endl ;
-                break;
-            case 6 :
-                cout << "square root: " << sqrt(num1) <<";" << sqrt(num2) << endl;
-                break;
-            case 7 :
-                cout << "Do you really want to exit? (1 = Yes, 0 = No): ";
-                cin >> ToContinue;
-                ToContinue = !ToContinue;
-                break;
-        }
-    } while(ToContinue);
-
+    double num1 = 0;
+    double num2 = 0;
+    cout << "select an operation" << endl;
+    cout << "1.sum" << endl;
+    cout << "2.difference" << endl;
+    cout << "3.multiplication" << endl;
+    cout << "4.division" << endl;
+    cout << "5.square" << endl;
+    cout << "6.square root" << endl;
+    cin >> operation;
+    cout << "Enter the numbers with which you want to perform the operation:" << endl;
+    cin >> num1 >> num2;
+    switch (operation) {
+        case 1 :
+            cout << "sum: "<< num1 + num2 << endl;
+            break;
+        case 2 :
+            cout << "difference: " << num1 - num2 << endl;
+            break;
+        case 3 :
+            cout << "multiplication: " <<num1 * num2 << endl;
+            break;
+        case 4 :
+            cout << "division: " << num1 / num2 << endl;
+            break;
+        case 5 :
+            cout << "square: " << pow(num1,2) <<";" << pow(num2,2) << endl ;
+            break;
+        case 6 :
+            cout << "square root: " << sqrt(num1) <<";" << sqrt(num2) << endl;
+            break;
+    }
     return 0;
 }
