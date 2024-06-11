1552053f0a22a298ec2174dd833bcb5f9e6bb060
Your Name <you@example.com>
2024-06-11 20:58:35 +0300

    Addet fourth task for first lection

--- Lesson_1/Task_4/main.cpp
+++ Lesson_1/Task_4/main.cpp
@@ -8,6 +8,8 @@ int main()
     int operation = 0;
     double num1 = 0;
     double num2 = 0;
+    cout << "Enter the numbers with which you want to perform the operation:" << endl;
+    cin >> num1 >> num2;
     cout << "select an operation" << endl;
     cout << "1.sum" << endl;
     cout << "2.difference" << endl;
@@ -16,8 +18,6 @@ int main()
     cout << "5.square" << endl;
     cout << "6.square root" << endl;
     cin >> operation;
-    cout << "Enter the numbers with which you want to perform the operation:" << endl;
-    cin >> num1 >> num2;
     switch (operation) {
         case 1 :
             cout << "sum: "<< num1 + num2 << endl;
@@ -29,14 +29,22 @@ int main()
             cout << "multiplication: " <<num1 * num2 << endl;
             break;
         case 4 :
-            cout << "division: " << num1 / num2 << endl;
-            break;
+            if (num2 !=0){
+                cout << "division: " << num1 / num2 << endl;
+                break;
+            }
+            else {
+                cout << " cannot be divided by zero"<< endl;
+                break;
+            }
         case 5 :
             cout << "square: " << pow(num1,2) <<";" << pow(num2,2) << endl ;
             break;
         case 6 :
             cout << "square root: " << sqrt(num1) <<";" << sqrt(num2) << endl;
             break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
     }
     return 0;
 }
